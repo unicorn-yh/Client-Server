f1b4cecf175d2f76d822f3de7be54ac6745462d7
unicornftk <laiyunghwei@gmail.com>
2022-08-27 19:03:16 +0800

    Create mylabel.cpp

---
+++ ImChat/Sources/mylabel.cpp
@@ -0,0 +1,17 @@
+#include "mylabel.h"
+
+myLabel::myLabel(QWidget * parent):QLabel(parent)
+{
+    //connect( this, SIGNAL( clicked() ), this, SLOT( slotClicked() ) );
+}
+
+void myLabel::slotClicked()
+{
+    qDebug()<<"Clicked";
+}
+
+void myLabel::mousePressEvent (QMouseEvent * event)
+{
+    emit Mouse_Pressed();
+}
+s
